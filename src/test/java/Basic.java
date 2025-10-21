f8306f0f6cf1be45bb8b88a961b5627978eb4cfb
Administrator <sibusiso.nkwanyana@simplify.africa>
2025-10-21 19:35:14 +0200

    Updated the app path and inspected elements

--- src/test/java/Basic.java
+++ src/test/java/Basic.java
@@ -1,5 +1,6 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -15,22 +16,29 @@ public class Basic {
     public void setUp() throws MalformedURLException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
 //        capabilities.setCapability("platformVersion", "11.0"); // Replace with your emulator's version
-        capabilities.setCapability("deviceName", "Android Emulator");
-        capabilities.setCapability("app", "src/main/ApiDemos-debug.apk"); // Replace with the path to your APK file
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("app",System.getProperty("user.dir") + "/src/main/ApiDemos-debug.apk"); // Replace with the path to your APK file
         capabilities.setCapability("automationName", "UiAutomator2");
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/"), capabilities);
     }
 
     @Test
-    public void launchApp() {
+    public void launchApp() throws InterruptedException {
+
+        Thread.sleep(2000); // Wait for 2 seconds to ensure the app is fully loaded
+        driver.findElement(By.xpath("//android.widget.TextView[@content-desc='Accessibility']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.id("Custom View")).isDisplayed();
+
+        System.out.println(System.getProperty("user.dir") + "/src/main/ApiDemos-debug.apk");
         System.out.println("App launched successfully!");
     }
 
-    @AfterClass
-    public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
+//    @AfterClass
+//    public void tearDown() {
+//        if (driver != null) {
+//            driver.quit();
+//        }
+//    }
 }
