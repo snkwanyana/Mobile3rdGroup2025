be73878dae0d8912aaa783a38e9fce4059721db3
Administrator <sibusiso.nkwanyana@simplify.africa>
2025-10-28 18:35:14 +0200

    We implemented Given step definition by calling the initDriver method from MobileBase class

--- src/test/java/Stepsdefs/StepDefinition.java
+++ src/test/java/Stepsdefs/StepDefinition.java
@@ -1,12 +1,14 @@
 package Stepsdefs;
 
+import Utils.MobileBase;
 import io.cucumber.java.en.*;
 
-public class StepDefinition{
+public class StepDefinition extends MobileBase {
 
     @Given("I am on the animation test page")
     public void i_am_on_the_animation_test_page() {
-
+        initDriver();
+        apiDemosScreen1.clickOnAnimationOption();
     }
     @When("I click the Event option")
     public void i_click_the_event_option() {
